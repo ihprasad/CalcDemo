a58aef9cc193ee488e19b9bd05502ef906183f1a
Hari Prasad <indugu.hari.prasad@accenture.com>
2018-12-12 22:37:48 +0530

    Created a demo project

---
+++ src/main/java/com/accenture/lkm/calc/App.java
@@ -0,0 +1,18 @@
+package com.accenture.lkm.calc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println(doAddition(5.6,  4.4));
+    }
+    
+    public static double doAddition(double a, double b){
+    	return a + b;
+    }
+    
+}
---
+++ src/test/java/com/accenture/lkm/calc/AppTest.java
@@ -0,0 +1,26 @@
+package com.accenture.lkm.calc;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class AppTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testDoAddition() {
+		// fail("Not yet implemented");
+		double expected = 10;
+		double actual = 10;
+		double delta = 0;
+		
+		assertEquals(expected, actual, delta);
+		
+		
+	}
+
+}
